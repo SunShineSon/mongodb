cf15b52e86936b5dce4a7bb1788d2a780a3fa7b6
SunShineSon <ggl1314ggl@163.com>
2015-09-11 16:46:05 +0800

    1、项目分支调试。

--- mongodb/src/main/java/demo/string/StringDemo.java
+++ mongodb/src/main/java/demo/string/StringDemo.java
@@ -3,7 +3,9 @@ package demo.string;
 public class StringDemo {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
+		String uri = "www.baidu.com";
+		System.out.println(uri.startsWith(""));
+		
 
 	}
 
